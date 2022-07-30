360bbb62e6db26b725ea7f3d0e044a4326396c69
tnebes <tomislav.nebes@gmail.com>
2022-07-30 10:45:07 +0200

    SPLC small fix to the mediatype return

--- src/main/java/hr/tnebes/lovecalculator/controllers/impl/LoveCalculatorControllerImpl.java
+++ src/main/java/hr/tnebes/lovecalculator/controllers/impl/LoveCalculatorControllerImpl.java
@@ -4,6 +4,7 @@ import hr.tnebes.lovecalculator.constants.Constants;
 import hr.tnebes.lovecalculator.controllers.LoveCalculatorController;
 import hr.tnebes.lovecalculator.services.LoveCalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,7 +23,7 @@ public class LoveCalculatorControllerImpl implements LoveCalculatorController {
     private LoveCalculatorService loveCalculatorService;
 
     @Override
-    @GetMapping(GET_LOVE_PERCENT_PATH)
+    @GetMapping(path = GET_LOVE_PERCENT_PATH, produces = MediaType.TEXT_PLAIN_VALUE)
     public String getLovePercent(@RequestParam(value = "name1") final String name1, @RequestParam(value = "name2") final String name2) {
         return String.valueOf(loveCalculatorService.getLovePercent(name1, name2));
     }
